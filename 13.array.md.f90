b11a63cc9bc44763651ed05ded6ef841a1c6492e
Talha Balaj <talhabalaj@hotmail.com>
2020-10-11 13:03:34 +0500

    added functions and array

--- 13.array.md.f90
+++ 13.array.md.f90
@@ -6,7 +6,55 @@ program arrays
   integer, dimension(1:5) :: a1
   ! Create multidimensional array (Matrix)
   integer, dimension(5,5) :: a4
-
+  ! Dynamic Array  
   integer, dimension(:), allocatable :: a5
+  integer :: size_of_array
+
+  ! Helpers
+  integer :: n, x, y
+
+  do n = 1, 5
+    a1(n) = n * n
+  end do
+
+  do n = 1, 5
+    print 100, "-->", a1(n)
+    100 format (a5, i3)
+  end do
+
+  do x = 1, 5
+    do y = 1, 5
+      a4(x, y) = x * y
+    end do
+  end do
+
+  do x = 1, 5
+    do y = 1, 5
+      write (*, "(i5)", advance = "no") a4(x, y)
+    end do
+    write (*,*)
+  end do
+      
+  do x = 1, 5
+    print "(5i5)", (a4(x, y), y = 1, 5)
+  end do
+
+  print *, "phir kitne size ki chahiye?"
+  read *, size_of_array
+  allocate(a5(1:size_of_array))
+
+  do x = 1, size_of_array
+    a5(x) = 5 * x
+  end do
+
+  do x = 1, size_of_array
+    print "(i5)", a5(x)
+  end do
+
+  a1 = (/ 10, 12, 14, 16, 18 /)
+
+  do n = 1, 5
+    print *,  a1(n)
+  end do
 
 end program arrays
---
+++ 14.functions.md.f90
@@ -0,0 +1,39 @@
+program functions
+  implicit none
+
+  integer :: square, cube
+  print *, get_sum(1, 5)
+  print *, get_sum2(2, 3)
+  
+  call square_cube(5, square, cube)
+  print *, square, cube
+  
+  contains
+    integer function get_sum(n1, n2)
+      implicit none
+      integer :: n1, n2, sum
+      sum = n1 + n2
+    end function get_sum
+
+    function get_sum2(n1, n2) result(sum)
+      integer, intent(in) :: n1, n2
+      integer :: sum
+      sum = n1 + n2
+    end function get_sum2
+
+    subroutine square_cube(n, m_square, m_cube)
+      integer, intent(in) :: n
+      integer, intent(out) :: m_square, m_cube
+      m_square = n * n
+      m_cube = n * n * n
+    end subroutine square_cube
+
+    recursive function factorial(n) result(o)
+      integer :: n, o
+      if (n == 1) then 
+        o = 1
+      else
+        o = factorial(n - 1) * n
+      end if
+    end function
+end program functions
